3abeec1a5677e055a426b46e92c459f8a4f3b188
parthjoshi2403200524032005 <parthjohi009@gmail.com>
2025-08-01 10:28:43 +0530

    changes

--- utils/api.js
+++ utils/api.js
@@ -1,2 +1,6 @@
 // utils/api.ts
-export const API_BASE_URL = "http://localhost:5000"; // Replace with your Render URL in prod
+// export const API_BASE_URL = "http://localhost:5000"; // Replace with your Render URL in prod
+
+
+
+export const API_BASE_URL = "https://backend-peach-five-91.vercel.app";
